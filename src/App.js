cb856d63f2a865bb578af3246abe6e64ed8c922b
Fahd <64308102+fahd23@users.noreply.github.com>
2022-12-08 09:39:47 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { styled } from "@mui/material/styles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header/Header";
@@ -5,15 +6,21 @@ import CoinPage from "./Pages/CoinPage";
 import Homepage from "./Pages/Homepage";
 
 function App() {
+  const Root = styled("div")(({ theme }) => ({
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  }));
+
   return (
     <Router>
-      <div>
+      <Root>
         <Header />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/coins/:id" element={<CoinPage />} />
         </Routes>
-      </div>
+      </Root>
     </Router>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import {
+  AppBar,
+  Container,
+  createTheme,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
 function Header() {
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
   return (
-    <div>Header</div>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              sx={{
+                flex: "1",
+                color: "gold",
+                fontFamily: "Montserrat",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Cryto Hunter
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginLeft: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 }
 
-export default Header
+export default Header;
